20ad937892f0bdbf40a10ad973df0fe78d98d8cb
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-27 19:42:23 +0530

    assignments

---
+++ Assignment-1/Solution.java
@@ -0,0 +1,7 @@
+public class Solution 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Name : ravalish , Roll Number : 2186069 ");
+	}
+}
---
+++ Assignment-3/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Double degreesCelsius = scan.nextDouble();
+		// calculateFromDegreesToFarenheit(degreesCelsius);
+		// degreesCelsius = (degreesCelsius - 32)*1.8;
+		degreesCelsius = (degreesCelsius * (9.0/5.0))+32;
+		System.out.println(degreesCelsius);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+	// static void calculateFromDegreesToFarenheit(double dc);
+	// {
+	// 	dc = ((dc - 32)*5)/9;
+	// 	System.out.println(dc);
+
+	// }
+}
