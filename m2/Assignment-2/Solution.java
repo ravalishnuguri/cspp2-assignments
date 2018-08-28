73850e5cdbe63400c034a5573dcb8e3f2167ef7b
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-28 17:46:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -2,35 +2,36 @@ import java.util.Scanner;
 import java.lang.Math;
 final class Solution {
 
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 **/
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments.
+     **/
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	@param      x     { parameter_description }
-	@param      y     { parameter_description }
-	@param      z     { parameter_description }
-	**/
-	static void rootsOfQuadraticEquation(final int x, final int y, final int z)
-	{
-		double root1;
-		double root2;
-		double quad;
-		quad = Math.sqrt((y * y)-(4 * x * z));
-		root1 = ( - y + quad) / (2 * x );
-		root2 = ( - y - quad) / ( 2 * x);
-		// return root1,  root2; System.out.format("%.1f %.1f",root1,root2);
-		// System.out.println(root2);
-		System.out.println(root1+" "+root2);
-	}
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        roots(a, b, c);
+    }
+    /**
+    Need to write the rootsOfQuadraticEquation function and
+    print the output.
+    @param      x     { parameter_description }
+    @param      y     { parameter_description }
+    @param      z     { parameter_description }
+    **/
+    static void roots(final int x, final int y, final int z)
+    {
+        double root1;
+        double root2;
+        double quad;
+        quad = Math.sqrt((y * y)-(4 * x * z));
+        root1 = (- y + quad) / (2 * x);
+        root2 = (- y - quad) / (2 * x);
+        // return root1,  root2; System.out.format("%.1f %.1f",root1,root2);
+        // System.out.println(root2);
+        System.out.println(root1+" "+root2);
+    }
 }
