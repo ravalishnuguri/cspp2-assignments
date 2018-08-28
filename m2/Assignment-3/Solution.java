0b957fc8e71d4e3122c7a296bf3d3fe665ad8429
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-28 14:39:50 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- m2/Assignment-3/Solution.java
+++ m2/Assignment-3/Solution.java
@@ -6,8 +6,8 @@ public class Solution {
 	*/
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);      
-        int base = s.nextInt();
-        int exponent = s.nextInt();
+        int base=s.nextInt();
+        int exponent=s.nextInt();
         int result=power(base,exponent);
         System.out.println(result);
 	}
@@ -16,9 +16,9 @@ public class Solution {
 	*/
 	static int power(int a, int b)
 	{
-		if(b != 0)
+		if(b!=0)
 		{
-			return (a * (power(a, b-1)));
+			return (a*(power(a, b-1)));
 		}
 		else
 		{
