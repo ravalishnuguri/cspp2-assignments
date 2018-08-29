d3ae601ed5bf0a080f2881f5c59ba4bc2be57c3d
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-29 14:53:38 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -14,10 +14,10 @@ final class Solution {
      **/
     public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        gcd(n1,n2);
+        gcd(n1, n2);
     }
     /**
     Need to write the gcd function and print the output.
@@ -26,7 +26,7 @@ final class Solution {
     **/
     static void gcd(final int num1, final int num2) {
         int g = 0;
-        for(int i = 1;i <= num1 && i <= num2;i++) {
+        for(int i = 1; i <= num1 && i <= num2; i++) {
             if(num1 % i == 0 && num2 % i == 0) {
                 g = i;
             }
