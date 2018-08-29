af45d73df9112255c3fc5e7e97fa8fc4dcd326eb
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-29 14:50:52 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -1,28 +1,28 @@
-
 import java.util.Scanner;
 /*
  Do not modify this main function.
 */
 final class Solution {
-    private Solution() { }
-
     /**
-     * { function_description }
+     * class for finding gcd.
+     **/
+    private Solution() { }
+    /**
+     * function_description.
      *
-     * @param      args  The arguments
+     * @param      args  The arguments.
      **/
     public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s=new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1,n2);
     }
     /**
     Need to write the gcd function and print the output.
-    
-    @param      num1  The number 1
-    @param      num2  The number 2
+    @param      num1  The number 1.
+    @param      num2  The number 2.
     **/
     static void gcd(final int num1, final int num2) {
         int g = 0;
