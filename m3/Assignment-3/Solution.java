184f3302d0c24ea4e3bd8bba0a541be240c18034
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-29 14:46:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -1,24 +1,33 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
-public class Solution {
+ Do not modify this main function.
+*/
+final class Solution {
+    private Solution() { }
 
-    public static void main(String[] args) {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     **/
+    public static void main(final String[] args) {
 
         Scanner s=new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1,n2);
     }
-    /*
-	Need to write the gcd function and print the output.
-	*/
-    static void gcd(int num1, int num2) {
-        int g=0;
-        for(int i=1;i<=num1&&i<=num2;i++) {
-            if(num1%i==0&&num2%i==0) {
+    /**
+    Need to write the gcd function and print the output.
+    
+    @param      num1  The number 1
+    @param      num2  The number 2
+    **/
+    static void gcd(final int num1, final int num2) {
+        int g = 0;
+        for(int i = 1;i <= num1 && i <= num2;i++) {
+            if(num1 % i == 0 && num2 % i == 0) {
                 g = i;
             }
         }
