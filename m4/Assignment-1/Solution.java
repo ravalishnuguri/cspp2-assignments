6cb6458d439c81a1014c49d4e75c125f8a07c3f8
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-30 12:27:29 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,20 +1,27 @@
-
-import java.util.*;
-public class Solution
-{/*
-	Fill this main function to print maximum of given array
-	*/
-	public static void main(String[] args)
+import java.util.Scanner;
+/** import for taking input
+**/
+final class Solution {
+/**
+ Fill this main function to print maximum of given array.
+**/
+	private Solution() { }
+	/**
+	init method for Solotion.
+	
+	@param      args  The arguments.
+	**/
+	public static void main(final String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		int[] arr = new int[n];
-		for (int i=0;i<arr.length;i++) {
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
 		int max = arr[0];
-		for(int i=0; i<arr.length;i++) {
-			if(arr[i] > max) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
 				max = arr[i];
 			}
 		}
