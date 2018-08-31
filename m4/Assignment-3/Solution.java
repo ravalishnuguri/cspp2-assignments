4a7311fa73e9366262ebc80d67e7006473ad8395
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-31 18:04:06 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -13,6 +13,7 @@ final class Solution {
 */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
         int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < n; i++) {
             String s = sc.nextLine();
