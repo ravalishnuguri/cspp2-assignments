a97c12d23338c015387e3d64748f7bfc5c7f8ea3
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-31 14:19:14 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-/** Given an String, Write a java method that returns the decimal value for the given binary string.
+/** Given an String, Write a java method that returns the
+decimal value for the given binary string.
 **/
 final class Solution {
     /**init method for Solution class.
