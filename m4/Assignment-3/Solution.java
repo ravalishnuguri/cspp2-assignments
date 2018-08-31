466e13add6eaeea6f9f63d6b854ee3f4c3dc63c9
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-31 16:55:19 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -1,24 +1,37 @@
-// Given an String, Write a java method that returns the decimal value for the given binary string.
 import java.util.Scanner;
-public class Solution
-{/*
-    Do not modify this main function.
-    */
-    public static void main(String[] args)
+/** Given an String, Write a java method that returns the decimal value for the given binary string.
+**/
+final class Solution {
+    /**init method for class Solution
+    **/
+    private Solution() { }
+/**
+ Do not modify this main function.
+
+ @param  args  The arguments.
+*/
+    public static void main(final String[] args)
     {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        for(int i=0;i<n;i++){
-            String s=sc.nextLine();
-            String res=binaryToDecimal(s);//Write binaryToDecimal function
+        for(int i = 0; i < n; i++){
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);//Write binaryToDecimal function
             System.out.println(res);
         }
     }
-    static String binaryToDecimal(String str) {
+    /**
+     * { function_description }
+     *
+     * @param      str   The string
+     *
+     * @return     { description_of_the_return_value }
+     **/
+    static String binaryToDecimal(final String str) {
         int k = 0;
-        for (int j = 0; j<str.length();j++) {
+        for (int j = 0; j < str.length(); j++) {
             if (str.charAt(j) == '1') {
-                k = (int) (k + Math.pow(2,str.length() - 1 - j));
+                k = (int) (k + Math.pow(2, str.length() - 1 - j));
             }
         }
         return k + "";
