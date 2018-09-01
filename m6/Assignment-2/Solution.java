c0d6652da4594305b35e0a35a6b6c5631b24967a
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-01 10:58:07 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m6/Assignment-2/Solution.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+/**
+ * Write a java program to round the
+ * elements of a matrix to the nearest 100.
+ *
+ * @author : 
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * Function to round the
+     * elements of a matrix to the nearest 100.
+     *
+     * @param      a     Matrix of which the elements to be rounded
+     * @param      rows     Number of rows
+     * @param      columns     Number of columns
+     *
+     * @return     Matrix of the rounded elements
+     */
+    static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
+
+	// write ypur code here
+        int size =11, m = 100, mid = 50;
+        int mul[] = new int[size];
+        for(int i = 0; i < size; i++) {
+            mul[i] = i * m;
+        }
+        int temp = 0;
+        for(int j =0; j<rows;j++) {
+            for (int k=0;k<columns ;k++ ) {
+                for (int i = 0;i<mul.length;i++) {
+                    temp = Math.round(a[j][k] - mul[i]);
+                    if (temp < mid) {
+                        a[i][j] = mul[k];
+                        break;
+                    } else if (temp == mid) {
+                        a[j][k] = mul[i+1];
+                        break;
+                    }
+                }
+                
+            }
+        }
+        return a;
+	
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+        int[][] b = roundHundred(a, m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println(b[i][n - 1]);
+        }
+    }
+}
