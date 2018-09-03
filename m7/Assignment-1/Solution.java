0d41af3764308afef65f8a76a56846c07f780a9c
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-03 17:48:13 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

---
+++ m7/Assignment-1/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class InputValidator {
+	String a;
+	public InputValidator(String str) {
+		a = str;
+	}
+	public boolean validateData() {
+		if (a.length()<=6) {
+			// System.out.println("True");
+			return true;
+		}
+		else {
+			// System.out.println("False");
+			return false;
+		}
+	}
+	/*Write the atrributes and methods for InputValidator*/
+}
+public class Solution
+{
+	public static void main(String args[])
+    {
+    	Scanner s=new Scanner(System.in);
+    	String input=s.next();
+    	InputValidator i=new InputValidator(input);    	
+    	System.out.println(i.validateData());
+
+    }
+
+}
