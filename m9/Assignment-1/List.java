3acdf95ab49e7c80806fe53613c5e84322f3ec10
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-05 11:08:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m9/Assignment-1/List.java
+++ m9/Assignment-1/List.java
@@ -78,6 +78,10 @@ public class List {
         // That is the initial value to use for size.
         size = 0;
     }
+    public void List(int n) {
+        size = 0;
+        list = new int[n];
+    }
 
     /*
      * Overloaded constructor with list capacity as argument
@@ -95,10 +99,6 @@ public class List {
      */
 
     // todo - add an overloaded constructor here
-    public List(int capacity) {
-        size = 0;
-        list = new int[capacity];
-    }
 
     
     /*
