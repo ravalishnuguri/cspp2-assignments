192d736bc3850fb314a68efdc84011960b40ea68
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-06 10:24:43 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- m10/Assignment-2/Solution.java
+++ m10/Assignment-2/Solution.java
@@ -18,7 +18,7 @@ final class Solution {
     public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl;
+        StringListInterface l = (StringListInterface) sl;
         //Typecasting is done for StringListInterface
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
