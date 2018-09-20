fba2d2d5e6bd540b56b1b94c2276420a1053a63f
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-09-20 14:29:40 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- m23/Assignment-1/Solution.java
+++ m23/Assignment-1/Solution.java
@@ -5,11 +5,14 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
-		File file = new File(s);
-        File[] files = file.listFiles();
-        for(File f: files){
-            System.out.println(f.getName());     
-        }
-
+		if (s== null){
+			System.out.println("empty directory");
+		}else {
+			File file = new File(s);
+        	File[] files = file.listFiles();
+        	for(File f: files){
+            System.out.println(f.getName());
+        	}
+		}
 	}
 }
